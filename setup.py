aa25ec3d0091a976e18fd61ce3268e46a2b8202c
ecchoe <107980405+ecchoe@users.noreply.github.com>
2023-02-14 03:40:09 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='pytorch_convolutional_rnn',
+    packages=find_packages(),
+    version='0.1.0',
+    description='pytorch convolutional rnn',
+    author='Me',
+    license='none',
+)
